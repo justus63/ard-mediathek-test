d87d8e6cc79bddd9fac2b56e58595bdc050fcb8f
justus63 <justuscheinz@gmail.com>
2025-07-23 12:01:28 +0200

    Webseite betreten

--- cypress/e2e/ardtest.cy.js
+++ cypress/e2e/ardtest.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('ARD Mediathek Suche – Tagesschau Flow', () => {
+  const baseUrl = 'https://www.ardmediathek.de';
+  const tagesschauSendungUrl = `${baseUrl}/sendung/tagesschau/Y3JpZDovL2Rhc2Vyc3RlLmRlL3RhZ2Vzc2NoYXU`;
+
+  before(() => {
+    cy.visit(baseUrl);
+  });
+
+  
+});
